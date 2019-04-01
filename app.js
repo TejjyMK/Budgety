6438d62c127975bc8e1e2edfd3a5259a901c7d68
Olateju Ati-John <kantejj.atijohn@gmail.com>
2019-04-01 17:45:10 +0100

    implemented the module pattern

---
+++ app.js
@@ -0,0 +1,14 @@
+// * Module that handles the budget data
+// IIFE
+var budgetController = (function () {
+
+})();
+
+var UIController = (function(){
+
+})();
+
+var controller = (function (budgetCtrl, UICtrl) {
+
+    // ? passing the other 2 modules as arguments
+})(budgetController, UIController);
