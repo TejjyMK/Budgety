f43e352ffd66bb342d5931054ec19aeff4511400
TejjyMK <kantejj.atijohn@gmail.com>
2019-05-21 03:14:47 +0100

    created income and expense function constructors

--- app.js
+++ app.js
@@ -2,6 +2,33 @@
 // IIFE
 var budgetController = (function () {
 
+    var Expense = function(id, description, value){
+        this.id = id;
+        this.description = description;
+        this.value= value;
+    };
+
+    var Income = function(id, description, value){
+        this.id = id;
+        this.description = description;
+        this.value= value;
+    };
+
+    var allExpenses = [];
+    var allIncomes = [];
+    var totalExpenses = 0;
+
+    var data = {
+        allItems:{
+            exp: [],
+            inc: []
+        },
+        total: {
+            exp: 0,
+            inc: 0
+
+        }
+    }
 })();
 
 // * UI Controller
