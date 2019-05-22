93bccce82e106271635e7cf0f328e6279ac63b7f
Olateju Ati-John <14042256@brookes.ac.uk>
2019-05-22 06:19:49 +0100

    validity checks for input

--- app.js
+++ app.js
@@ -78,14 +78,14 @@ var UIController = (function () {
             return {
                 type: document.querySelector(DOMinputs.inputType).value, // * will be either inc or exp
                 description: document.querySelector(DOMinputs.inputDescription).value,
-                value: document.querySelector(DOMinputs.inputValue).value
+                value: parseFloat(document.querySelector(DOMinputs.inputValue).value) // ? this will convert a string to float
             }
 
         },
 
         addList: function (obj, type) {
             // ? create html string with placeholder text
-            var html, newHtml,element;
+            var html, newHtml, element;
 
             if (type === 'inc') {
                 element = DOMinputs.incomeContainer;
@@ -120,21 +120,21 @@ var UIController = (function () {
 
             // ? Insert the HTML into the DOM
 
-            document.querySelector(element).insertAdjacentHTML('beforeend',newHtml);
+            document.querySelector(element).insertAdjacentHTML('beforeend', newHtml);
 
         },
 
-        clearFields: function(){
+        clearFields: function () {
             var fields, fieldsArr;
 
-            fields = document.querySelectorAll(DOMinputs.inputDescription+', '+ DOMinputs.inputValue);
+            fields = document.querySelectorAll(DOMinputs.inputDescription + ', ' + DOMinputs.inputValue);
 
             // ? converting the list into an Array
             fieldsArr = Array.prototype.slice.call(fields);
 
             // ? clears the fields
             fieldsArr.forEach(function (current, index, array) {
-                current.value="";
+                current.value = "";
             });
 
             // ? so that it focuses on the description bit
@@ -163,25 +163,37 @@ var controller = (function (budgetCtrl, UICtrl) {
         });
 
     };
+
+    var updateBudget = function () {
+        // * 4) Calculate the budget
+        // * 5) Return the budget
+        // * 6) Display the budget on the UI
+    };
+
     var ctrlAddItem = function () {
         var input, newItem;
         // * 1) Get the field input data
 
         input = UICtrl.getInput();
         console.log(input);
-        // * 2) Add the item to the budget controller
 
-        newItem = budgetCtrl.addItem(input.type, input.description, input.value);
-        // * 3) Add the item to the UI
+        // ? Validity checks
+        if(input.description !== "" && !isNaN(input.value) && input.value >0){
+            // * 2) Add the item to the budget controller
 
-        UICtrl.addList(newItem, input.type);
+            newItem = budgetCtrl.addItem(input.type, input.description, input.value);
+            // * 3) Add the item to the UI
 
-        // ? Gotta clear the fields
-        UICtrl.clearFields();
+            UICtrl.addList(newItem, input.type);
+
+            // ? Gotta clear the fields
+            UICtrl.clearFields();
+
+            // * 4) Calculate and update the budget
+            updateBudget();
+        }
 
 
-        // * 4) Calculate the budget
-        // * 5) Display the budget on the UI
     };
 
     return {
