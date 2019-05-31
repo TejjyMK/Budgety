0b1c0eb58065ffc479112ca9e08a8d4e833e4ff6
Olateju Ati-John <14042256@brookes.ac.uk>
2019-05-31 03:39:27 +0100

    ability to deleted item from data structure

--- app.js
+++ app.js
@@ -66,6 +66,19 @@ var budgetController = (function () {
 
             return newItem;
         },
+        deleteItem: function (type, id) {
+            var ids, index;
+            ids = data.allItems[type].map(function (current) {
+                return current.id;
+            });
+
+            index = ids.indexOf(id);
+
+            if (index !== -1)
+                data.allItems[type].splice(index, 1)
+
+        }
+        ,
         calculateBudget: function () {
             // ? calculate total income and expenses
             calculateTotal('exp');
@@ -133,7 +146,7 @@ var UIController = (function () {
 
             if (type === 'inc') {
                 element = DOMinputs.incomeContainer;
-                html = '  <div class="item clearfix" id="income-%id%">\n' +
+                html = '  <div class="item clearfix" id="inc-%id%">\n' +
                     '       <div class="item__description">%description%</div>\n' +
                     '       <div class="right clearfix">\n' +
                     '            <div class="item__value">+ %value%</div>\n' +
@@ -144,7 +157,7 @@ var UIController = (function () {
                     '    </div>';
             } else if (type === 'exp') {
                 element = DOMinputs.expensesContainer;
-                html = '<div class="item clearfix" id="expense-%id%">\n' +
+                html = '<div class="item clearfix" id="exp-%id%">\n' +
                     '       <div class="item__description">%description%</div>\n' +
                     '       <div class="right clearfix">\n' +
                     '            <div class="item__value">- %value%</div>\n' +
@@ -261,15 +274,16 @@ var controller = (function (budgetCtrl, UICtrl) {
     };
 
     var ctrlDeleteItem = function (event) {
-        var itemID,splitID, type, ID;
+        var itemID, splitID, type, ID;
         itemID = event.target.parentNode.parentNode.parentNode.parentNode.id;
 
-        if(itemID){
-          splitID = itemID.split('-');
-          type= splitID[0];
-          ID= splitID[1];
-
+        if (itemID) {
+            splitID = itemID.split('-');
+            type = splitID[0];
+            // ? if not it reads as a string
+            ID = parseInt(splitID[1]);
             // * 1 Delete the item from the data structure
+            budgetCtrl.deleteItem(type, ID);
 
             // * 2 Delete the item from the UI
 
